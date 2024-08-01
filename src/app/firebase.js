9810a353b31f14f851933ed08d81638c967dd10b
GeoSauer <geoffrey.sauer89@gmail.com>
2024-08-01 15:44:26 -0600

    add basic CRUD and main page

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,25 +1,35 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpUdXL8dHmZC4SNXme8SWn0tmc3fA8euo",
-  authDomain: "inventory-management-app-e7ff5.firebaseapp.com",
-  projectId: "inventory-management-app-e7ff5",
-  storageBucket: "inventory-management-app-e7ff5.appspot.com",
-  messagingSenderId: "1048946366565",
-  appId: "1:1048946366565:web:e06a87f31e7476a4a38e8c",
-  measurementId: "G-FF23KH33K4",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-const analytics = getAnalytics(app);
+
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported()
+    .then((supported) => {
+      if (supported) {
+        analytics = getAnalytics(app);
+      } else {
+        console.warn("Firebase Analytics is not supported in this environment.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error checking Firebase Analytics support:", error);
+    });
+}
 
 export { firestore, analytics };
